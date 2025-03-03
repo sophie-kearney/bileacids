3732ad892d8ba8dee867a6130dbb5aa79a93e625
Sophie Kearney <kearney.sophie@yahoo.com>
2025-03-03 14:37:56 -0500

    Cumulative incidence.py

--- src/RNN_models/cumulative_incidence.py
+++ src/RNN_models/cumulative_incidence.py
@@ -19,7 +19,7 @@ trained_model = "MaskedGRU_0.8215"
 ###
 
 predictions = pd.read_csv(f"processed/{cohort}/{trained_model}_predictions.csv")
-print(predictions)
+# print(predictions)
 
 ###
 # FIT DATA
@@ -28,12 +28,17 @@ print(predictions)
 kmf = KaplanMeierFitter()
 plt.figure(figsize=(8, 6))
 
+colors = {
+    "High": "#d8b709",
+    "Low": "#0c775e"
+}
+
 for group in predictions["RiskGroup"].unique():
     mask = predictions["RiskGroup"] == group
     kmf.fit(predictions.loc[mask, "ADConversionTime"].fillna(predictions["ADConversionTime"].max()), event_observed=predictions.loc[mask, "AD"])
-    kmf.plot_survival_function(label=group)
+    kmf.plot_survival_function(label=group, color=colors.get(group, "#000000"))
 
-plt.title("Kaplan Meier Curve of pMCIiAD")
+plt.title("Kaplan Meier Curve of MCI to AD Conversion")
 plt.xlabel("Time (months)")
 plt.ylabel("Survival Probability")
 plt.legend(title="Risk Group")
